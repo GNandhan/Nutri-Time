e552d1f5f4ed0d084611d1cb22a3f574d8452ad4
Gowrinandhan S <gowrinandhan95@gmail.com>
2024-03-14 17:05:48 +0530

    no changes

--- Admin/pages/admin-dashboard.php
+++ Admin/pages/admin-dashboard.php
@@ -45,16 +45,20 @@ if ($row = mysqli_fetch_assoc($query)) {
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
-          <div class="row">
-            <div class="col-md-12 grid-margin">
-              <div class="row">
-                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold">Welcome <?php echo $AdminName; ?> </h3>
-                  <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span class="text-primary">3 unread alerts!</span></h6>
+        <div class="row">
+        <div class="col-md-12 grid-margin">
+                <div class="row">
+                        <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                                <h3 class="font-weight-bold">Welcome <?php echo $AdminName; ?> </h3>
+                                <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span class="text-primary">3 unread alerts!</span></h6>
+                        </div>
+                        <div class="col-12 col-xl-4 mb-4 mb-xl-0 text-right">
+                                <button class="btn btn-primary">Export</button>
+                        </div>
                 </div>
-              </div>
-            </div>
-          </div>
+        </div>
+</div>
+
           <div class="row">
             <!-- newly arrived product carousel -->
             <div class="col-md-6 grid-margin stretch-card">
