a5d4f0e64d22be59bb3143676a692bdc31db6e86
GNandhan <gowrinandhan95@gmail.com>
2024-04-15 02:30:55 +0530

    Simplified admin-attendence.php by removing unnecessary links, plugins, and codes.

--- Admin/pages/admin-attendence.php
+++ Admin/pages/admin-attendence.php
@@ -17,16 +17,11 @@
   <!-- plugins:css -->
   <link rel="stylesheet" href="../vendors/feather/feather.css">
   <link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- inject:css -->
   <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
-  <!-- endinject -->
   <link rel="shortcut icon" href="../images/icon-small.png" />
 </head>
 <body>
   <div class="container-scroller">
-    <!-- partial:../../partials/_navbar.html -->
 <!-- including the sidebar,navbar -->
 <?php
   include './topbar.php';
@@ -67,34 +62,6 @@
                         <td>Fat reducer</td>
                         <td><input type="checkbox"></td>
                       </tr>
-                      <tr>
-                        <td class="py-1">#00A001</td>
-                        <td>username 1</td>
-                        <td>Customer 1</td>
-                        <td>Fat reducer</td>
-                        <td><input type="checkbox"></td>
-                      </tr>
-                      <tr>
-                        <td class="py-1">#00A001</td>
-                        <td>username 1</td>
-                        <td>Customer 1</td>
-                        <td>Fat reducer</td>
-                        <td><input type="checkbox"></td>
-                      </tr>
-                      <tr>
-                        <td class="py-1">#00A001</td>
-                        <td>username 1</td>
-                        <td>Customer 1</td>
-                        <td>Fat reducer</td>
-                        <td><input type="checkbox"></td>
-                      </tr>
-                      <tr>
-                        <td class="py-1">#00A001</td>
-                        <td>username 1</td>
-                        <td>Customer 1</td>
-                        <td>Fat reducer</td>
-                        <td><input type="checkbox"></td>
-                      </tr>
                     </tbody>
                   </table>
                 </div>
@@ -119,21 +86,8 @@
     <!-- page-body-wrapper ends -->
   </div>
   <!-- container-scroller -->
-  <!-- plugins:js -->
   <script src="../vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="../vendors/chart.js/Chart.min.js"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
   <script src="../js/off-canvas.js"></script>
-  <script src="../js/hoverable-collapse.js"></script>
-  <script src="../js/template.js"></script>
-  <script src="../js/settings.js"></script>
-  <script src="../js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="../js/chart.js"></script>
-  <!-- End custom js for this page-->
+  <script src="../js/template.js"></script>  
 </body>
 </html>
