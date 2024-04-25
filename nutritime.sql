999041a6ba949896997a9dc91a29f50eea3c5552
GNandhan <gowrinandhan95@gmail.com>
2024-04-25 13:21:20 +0530

    Updated database

--- nutritime.sql
+++ nutritime.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2024 at 09:14 AM
+-- Generation Time: Apr 25, 2024 at 09:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
