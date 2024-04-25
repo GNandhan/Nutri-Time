b2ba6239a2150df9a4d38544c4e6aef2fe964624
GNandhan <gowrinandhan95@gmail.com>
2024-04-25 14:03:17 +0530

    Updates database

--- nutritime.sql
+++ nutritime.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2024 at 09:50 AM
+-- Generation Time: Apr 25, 2024 at 10:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -197,7 +197,7 @@ CREATE TABLE `price` (
 INSERT INTO `price` (`pri_id`, `pro_name`, `pro_code`, `pro_category`, `pro_subcat`, `pro_mrp`, `pro_price`, `pro_dis15`, `pro_dis25`, `pro_dis35`, `pro_dis42`, `pro_dis50`, `pro_vp`, `pro_vptotal`, `pro_scoop`, `pro_scoop15`, `pro_scoop25`, `pro_scoop35`, `pro_scoop42`, `pro_scoop50`, `pro_quantity`, `pro_curquantity`, `pro_hsn`, `pro_img`, `pro_associate`, `pro_association`) VALUES
 (1, 'Formula 1', '1001', 'Targeted Nutrition', 'Brain Health', '2378', '2200', '2065', '1860', '1650', '1505', '1340', '12.75', '127.5', '20', '103.25', '93', '82.50', '75.25', '68', '120', '10', '986587548', '', 'Ashraf', ''),
 (2, 'Fiber Complex', '1002', 'Targeted Nutrition', 'Energy', '2792', '2600', '2425', '2180', '1940', '1765', '1570', '22.45', '224.5', '30', '80', '72', '64', '58', '52', '20', '15', '98658754', '', 'Ashraf', ''),
-(3, 'Protein', '1003', 'Targeted Nutrition', 'Energy', '1413', '1200', '1230', '1105', '980', '895', '795', '12.75', '144', '30', '41', '36.83', '32.67', '29.83', '26.50', '12', '12', '221144', '', 'Ashraf', 'Nutritime'),
+(3, 'Protein', '1003', 'Targeted Nutrition', 'Energy', '1413', '1200', '1230', '1105', '980', '895', '795', '12.75', '144', '30', '41', '36.83', '32.67', '29.83', '26.50', '12', '-2', '221144', '', 'Ashraf', 'Nutritime'),
 (4, 'Shakemate', '1004', 'Targeted Nutrition', 'Energy', '712', '700', '660', '625', '590', '565', '535', '15.95', '300', '18', '36.67', '34.72', '32.78', '31.39', '29.72', '20', '20', '562345', '', '', ''),
 (5, 'Afresh', '1005', 'Targeted Nutrition', 'Energy', '885', '800', '770', '690', '615', '560', '500', '22.45', '763.3', '25', '30.80', '27.60', '24.60', '22.40', '20', '64', '48', '9865875498', '', '', ''),
 (6, 'Dino ', '1006', 'Ayurdevic Nutrition', 'Energy', '150', '100', '90', '80', '70', '60', '50', '23.70', '474', '30', '10', '20', '30', '40', '50', '30', '30', '986598', '', 'Amal', ''),
@@ -242,23 +242,25 @@ CREATE TABLE `sales` (
   `sales_dispri` varchar(11) NOT NULL,
   `sales_cus` varchar(100) NOT NULL,
   `sales_address` varchar(100) NOT NULL,
-  `sales_total` varchar(20) NOT NULL
+  `sales_total` varchar(20) NOT NULL,
+  `sales_assoc` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `sales`
 --
 
-INSERT INTO `sales` (`sales_id`, `sales_proid`, `sales_procode`, `sales_proname`, `sales_procat`, `sales_prosubcat`, `sales_mrp`, `sales_quan`, `sales_curquan`, `sales_vp`, `sales_vptotal`, `sales_gst`, `sales_dis`, `sales_dispri`, `sales_cus`, `sales_address`, `sales_total`) VALUES
-(4, 0, '1006', 'Dino ', 'Ayurdevic Nutrition', 'Energy', '150', '10', '', '23.70', '237', '18', '15', '590', 'Jeslin Biju', 'thbheththntrhtyhyh', '6962'),
-(5, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '8', '', '12.75', '102', '19', '15', '1860', 'Arun', 'iojdijewdeiwjdijewiodjewjifewriofoiref', '17707.2'),
-(7, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '10', '', '12.75', '127.5', '10', '25', '1105', 'zzzzzzzzzzzz', 'nor,,,,,,,,,,lllll', '12155'),
-(8, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '10', '', '12.75', '127.5', '18', '25', '1105', 'Jeslin Biju', 'yyyyyyyyyyyyyyyyyy', '13039'),
-(9, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '5', '', '12.75', '63.75', '11', '25', '1105', 'Jeslin Biju', 'thbheththntrhtyhyh', '6132.75'),
-(10, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '15', '', '12.75', '191.25', '10', '25', '1105', 'Arun', 'none', '18232.5'),
-(11, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '8', '', '12.75', '102', '17', '25', '1105', 'Arun', 'none', '10342.8'),
-(12, 0, '1001', 'Formula 1', 'Targeted Nutrition', 'Brain Health', '2378', '10', '', '12.75', '127.5', '5', '25', '1860', 'zzzzzzzzzzzz', 'yyyyyyyyyyyyyyyyyy', '19530'),
-(13, 0, '1002', 'Fiber Complex', 'Targeted Nutrition', 'Energy', '2792', '5', '', '22.45', '112.25', '45', '25', '2180', 'Jeslin Biju', 'iojdijewdeiwjdijewiodjewjifewriofoiref', '15805');
+INSERT INTO `sales` (`sales_id`, `sales_proid`, `sales_procode`, `sales_proname`, `sales_procat`, `sales_prosubcat`, `sales_mrp`, `sales_quan`, `sales_curquan`, `sales_vp`, `sales_vptotal`, `sales_gst`, `sales_dis`, `sales_dispri`, `sales_cus`, `sales_address`, `sales_total`, `sales_assoc`) VALUES
+(4, 0, '1006', 'Dino ', 'Ayurdevic Nutrition', 'Energy', '150', '10', '', '23.70', '237', '18', '15', '590', 'Jeslin Biju', 'thbheththntrhtyhyh', '6962', ''),
+(5, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '8', '', '12.75', '102', '19', '15', '1860', 'Arun', 'iojdijewdeiwjdijewiodjewjifewriofoiref', '17707.2', ''),
+(7, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '10', '', '12.75', '127.5', '10', '25', '1105', 'zzzzzzzzzzzz', 'nor,,,,,,,,,,lllll', '12155', ''),
+(8, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '10', '', '12.75', '127.5', '18', '25', '1105', 'Jeslin Biju', 'yyyyyyyyyyyyyyyyyy', '13039', ''),
+(9, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '5', '', '12.75', '63.75', '11', '25', '1105', 'Jeslin Biju', 'thbheththntrhtyhyh', '6132.75', ''),
+(10, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '15', '', '12.75', '191.25', '10', '25', '1105', 'Arun', 'none', '18232.5', ''),
+(11, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '8', '', '12.75', '102', '17', '25', '1105', 'Arun', 'none', '10342.8', ''),
+(12, 0, '1001', 'Formula 1', 'Targeted Nutrition', 'Brain Health', '2378', '10', '', '12.75', '127.5', '5', '25', '1860', 'zzzzzzzzzzzz', 'yyyyyyyyyyyyyyyyyy', '19530', ''),
+(13, 0, '1002', 'Fiber Complex', 'Targeted Nutrition', 'Energy', '2792', '5', '', '22.45', '112.25', '45', '25', '2180', 'Jeslin Biju', 'iojdijewdeiwjdijewiodjewjifewriofoiref', '15805', ''),
+(14, 0, '1003', 'Protein', 'Targeted Nutrition', 'Energy', '1413', '12', '', '12.75', '153', '18', '35', '980', 'Ashok', 'yyyyyyyyyyyyyyyyyy', '13876.8', 'Ashraf');
 
 -- --------------------------------------------------------
 
@@ -269,6 +271,7 @@ INSERT INTO `sales` (`sales_id`, `sales_proid`, `sales_procode`, `sales_proname`
 CREATE TABLE `shake` (
   `shake_id` int(11) NOT NULL,
   `shake_name` varchar(100) NOT NULL,
+  `shake_assoc` varchar(100) NOT NULL,
   `customer_id` int(10) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
   `shake_goal` varchar(100) NOT NULL,
@@ -287,16 +290,17 @@ CREATE TABLE `shake` (
 -- Dumping data for table `shake`
 --
 
-INSERT INTO `shake` (`shake_id`, `shake_name`, `customer_id`, `customer_name`, `shake_goal`, `shake_recipes`, `shake_mrp`, `shake_scoops`, `shake_extra`, `shake_extraprice`, `shake_discount`, `shake_expence`, `shake_total`, `shake_image`) VALUES
-(1, 'Club Shake', 0, 'Manu', 'Weight gainer', 'Formula 1,Protein,Shakemate', '0', '', 'Milk', '50', '15', '40', '90', 'main page.jpg'),
-(2, 'evening shake', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '', '', 'milk, sugar, protein', '50', '15', '10', '60', 'main page.jpg'),
-(3, 'evening shake', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '', '', 'milk, sugar, protein', '50', '15', '10', '60', 'main page.jpg'),
-(4, 'Club Shake222', 0, 'Manu', 'Weight gainer222', 'Formula 1,Protein', '0', '', 'water', '10', '50', '11', '21', 'main page.jpg'),
-(5, 'Club Shake222', 0, 'Manu', 'weight losser', 'Formula 1,Protein', '0', '', 'Milk', '20', '25', '40', '60', 'main page.jpg'),
-(6, 'Club Shake222', 0, 'Manu', 'Weight gainer222', 'Formula 1,Protein', '0', '', 'Milk', '50', '25', '10', '60', 'main page.jpg'),
-(7, 'Club Shake', 0, 'Manu', 'weight losser', 'Formula 1,Protein', '129.82999999999998', '', 'Milk', '50', '25', '60', '110', 'main page.jpg'),
-(8, 'Club Shake', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '129.82999999999998', '', 'Milk, Butter', '20', '25', '40', '189.83', 'main page.jpg'),
-(9, 'Club Shake', 0, 'Amal', 'Weight gainer222', 'Formula 1,Fiber Complex', '183.25', '', 'Milk, Butter', '20', '15', '50', '253.25', 'main page.jpg');
+INSERT INTO `shake` (`shake_id`, `shake_name`, `shake_assoc`, `customer_id`, `customer_name`, `shake_goal`, `shake_recipes`, `shake_mrp`, `shake_scoops`, `shake_extra`, `shake_extraprice`, `shake_discount`, `shake_expence`, `shake_total`, `shake_image`) VALUES
+(1, 'Club Shake', 'Ashraf', 0, 'Manu', 'Weight gainer', 'Formula 1,Fiber Complex,Shakemate', '219.92000000000002', '', 'Milk', '50', '15', '40', '90', 'bg2.jpg'),
+(2, 'evening shake', '', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '', '', 'milk, sugar, protein', '50', '15', '10', '60', 'main page.jpg'),
+(3, 'evening shake', '', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '', '', 'milk, sugar, protein', '50', '15', '10', '60', 'main page.jpg'),
+(4, 'Club Shake222', '', 0, 'Manu', 'Weight gainer222', 'Formula 1,Protein', '0', '', 'water', '10', '50', '11', '21', 'main page.jpg'),
+(5, 'Club Shake222', '', 0, 'Manu', 'weight losser', 'Formula 1,Protein', '0', '', 'Milk', '20', '25', '40', '60', 'main page.jpg'),
+(6, 'Club Shake222', '', 0, 'Manu', 'Weight gainer222', 'Formula 1,Protein', '0', '', 'Milk', '50', '25', '10', '60', 'main page.jpg'),
+(7, 'Club Shake', '', 0, 'Manu', 'weight losser', 'Formula 1,Protein', '129.82999999999998', '', 'Milk', '50', '25', '60', '110', 'main page.jpg'),
+(8, 'Club Shake', '', 0, 'Amal', 'weight losser', 'Formula 1,Protein', '129.82999999999998', '', 'Milk, Butter', '20', '25', '40', '189.83', 'main page.jpg'),
+(9, 'Club Shake', '', 0, 'Amal', 'Weight gainer222', 'Formula 1,Fiber Complex', '183.25', '', 'Milk, Butter', '20', '15', '50', '253.25', 'main page.jpg'),
+(10, 'Dino Shake', '', 0, 'Melow', 'weight losser', 'Formula 1,Fiber Complex,Shakemate', '199.72', '', 'milk boost, butter', '60', '25', '50', '309.72', 'bg3.jpg');
 
 -- --------------------------------------------------------
 
@@ -496,13 +500,13 @@ ALTER TABLE `program`
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `shake`
 --
 ALTER TABLE `shake`
-  MODIFY `shake_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `shake_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `staff`
